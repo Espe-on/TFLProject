47740210e1696176860eb14338aa5ad9b43a73b6
Lucille <lucilleablett@gmail.com>
2020-03-07 14:25:13 +0000

    Added Footer as component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import './App.scss';
-import {Header} from "./Components/Header/Header";
+
+import { Header } from './Components/Header/Header';
+import { Footer } from './Components/Footer/Footer';
 
 function App() {
     return (
-        <div className="App">
-            <Header/>
+        <div className='App'>
+            <Header />
+            <Footer />
         </div>
     );
 }
