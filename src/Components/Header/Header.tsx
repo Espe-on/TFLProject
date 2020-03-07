499c9b38f3a0858513fed501a80f8442a459322e
Lucille <lucilleablett@gmail.com>
2020-03-07 14:24:56 +0000

    Updated Header syntax

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -1,10 +1,10 @@
 import React from "react";
 import './Header.scss';
 
-export function Header () {
-    return(
-        <header className="App-header">
-            <h1>TFL API App!</h1>
-        </header>
-    )
+const Header = () => {
+    <header className="App-header">
+        <h1>TFL API App!</h1>
+    </header>
 }
+
+export default Header;
