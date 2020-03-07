752b2f6ca717d43117556b26d16420027d1702ce
Lucille <lucilleablett@gmail.com>
2020-03-07 14:30:31 +0000

    Added return statement

--- src/Components/Footer/Footer.tsx
+++ src/Components/Footer/Footer.tsx
@@ -3,11 +3,13 @@ import './Footer.scss'
 
 
 const Footer = () => {
-    <footer className='App-footer'>
-        <h1>
-            Created with ❤️ by Joseph & Lucille
-        </h1>
-    </footer>
+    return (
+        <footer className='App-footer'>
+            <h1>
+                Created with ❤️ by Joseph & Lucille
+            </h1>
+        </footer>
+    )
 }
 
 export default Footer;
--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -2,9 +2,11 @@ import React from "react";
 import './Header.scss';
 
 const Header = () => {
-    <header className="App-header">
-        <h1>TFL API App!</h1>
-    </header>
+    return (
+        <header className="App-header">
+            <h1>TFL API App!</h1>
+        </header>
+    )
 }
 
 export default Header;
