79d011ea2d14d22ff7a569b96a725761ab52a68c
espe-on <eljoe0@googlemail.com>
2020-03-07 19:37:47 +0000

    broke out Particles into separate component

--- src/App.tsx
+++ src/App.tsx
@@ -1,46 +1,18 @@
-import React, { Component } from 'react';
-import Particles from 'react-particles-js';
-
+import React from 'react';
 import Header from './Components/Header/Header';
 import Footer from './Components/Footer/Footer';
 import Map from './Components/Map/Map';
-
 import './App.scss';
+import { ParticlesContainer } from "./Components/Particles/ParticlesContainer";
 
-const particlesOptions = {
-    particles: {
-      number: {
-        value: 50,
-        density: {
-          enable:true,
-          value_area: 800
-        }
-      },
-      move: {
-        enable: true,
-        speed: 6
-      }
-    }
-  }
-
-class App extends Component {
-
-    // constructor() {
-    //     super();
-    // }
-
-    render() {
-        return (
-            <div className='App'>
-                {/*<Particles className='particles'
-                    params={particlesOptions}
-                />*/}
-                <Header />
-                    <Map />
-                <Footer />
-            </div>
-        );
-    }
+export default function App() {
+    return (
+        <div className='App'>
+            <ParticlesContainer/>
+            <Header/>
+            <Map/>
+            <Footer/>
+        </div>
+    );
 }
-
-export default App;
+    
