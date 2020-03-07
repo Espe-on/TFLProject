6947273f4011e08a8f726bba4c3f34a4e5f2c0d6
Lucille <lucilleablett@gmail.com>
2020-03-07 14:24:40 +0000

    Created app footer

---
+++ src/Components/Footer/Footer.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Footer.scss'
+
+
+const Footer = () => {
+    <footer className='App-footer'>
+        <h1>
+            Created with ❤️ by Joseph & Lucille
+        </h1>
+    </footer>
+}
+
+export default Footer;
