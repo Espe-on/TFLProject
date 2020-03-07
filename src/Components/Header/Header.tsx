b7557a3d0ab0d590ba037705f21915508a229e10
espe-on <eljoe0@googlemail.com>
2020-03-07 14:05:55 +0000

    Created Head Component

---
+++ src/Components/Header/Header.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import './Header.scss';
+
+export function Header () {
+    return(
+        <header className="App-header">
+            <h1>TFL API App!</h1>
+        </header>
+    )
+}
